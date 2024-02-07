455d2b4826f2fd80c5496f2816d3d58c8e558d46
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-02-07 19:10:09 +0100

    Update run_conan.py to work under Windows

--- run_conan.py
+++ run_conan.py
@@ -1,17 +1,29 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # Copyright (C) 2024 Roberto Rossini <roberros@uio.no>
 #
 # SPDX-License-Identifier: MIT
 
-import subprocess as sp
-import shutil
 import os
-import sys
+import platform
 import re
+import shutil
+import subprocess as sp
+import sys
 
 
-def find_conan():
+"""
+This script takes care of generating a valid conan profile and running conan install.
+Most of the complexity is due to Windows, as in this case we must use the compilers provided by the Rtools package.
+This means that:
+- We need to figure out Rtools' home folder
+- Inject the several bin folders into PATH
+- Detect compiler and cmake version
+- Make conan use the msys2 toolchain installed by Rtools
+"""
+
+
+def find_conan() -> str:
     conan = shutil.which("conan")
     if conan is None:
         raise RuntimeError(
@@ -22,31 +34,146 @@ def find_conan():
     return conan
 
 
+def get_conan_home() -> str:
+    return os.environ.get(
+        "CONAN_HOME", os.path.join(os.path.expanduser("~"), ".conan2")
+    )
+
+
+def get_rtools_home() -> str:
+    res = sp.check_output(
+        ["Rscript", "-e", "package_version(R.version)"], stderr=sp.DEVNULL
+    ).decode("utf-8")
+    matches = re.search(r"(\d+\.\d+).\d+", res)
+    if not matches:
+        raise RuntimeError("Unable to infer R version")
+
+    r_version = matches.group(1).replace(".", "")
+
+    rtools_home = os.path.join("C:\\", f"rtools{r_version}")
+
+    if not os.path.exists(rtools_home):
+        raise RuntimeError("Unable to find RTOOLS_HOME at: " + rtools_home)
+
+    return rtools_home
+
+
+def get_path_as_r() -> str:
+    res = sp.check_output(
+        ["Rscript", "-e", "Sys.getenv('PATH')"], stderr=sp.DEVNULL
+    ).decode("utf-8")
+    matches = re.search(r"\"(.*)\"", res, re.MULTILINE)
+    if not matches:
+        return ""
+
+    path = [os.path.normpath(p) for p in matches.group(1).split(";")]
+
+    rtools_home = get_rtools_home()
+
+    path = [
+        os.path.join(rtools_home, "usr", "bin"),
+        os.path.join(rtools_home, "mingw64", "bin"),
+    ] + path
+
+    return ";".join(path)
+
+
+def r_which(program: str) -> str:
+    res = sp.check_output(
+        ["Rscript", "-e", f'Sys.which("{program}")'], stderr=sp.DEVNULL
+    ).decode("utf-8")
+
+    matches = re.search(r"\n\"(.*)\"", res, re.MULTILINE)
+    if not matches:
+        return ""
+
+    return os.path.normpath(matches.group(1))
+
+
+def find_cc():
+    cc = r_which("gcc")
+
+    if cc == "":
+        cc = r_which("clang")
+
+    if cc == "":
+        raise RuntimeError("Unable to find a gcc or clang in your PATH")
+
+    return os.path.realpath(cc)
+
+
+def find_cxx():
+    cxx = r_which("g++")
+
+    if cxx == "":
+        cxx = r_which("clang++")
+
+    if cxx == "":
+        raise RuntimeError("Unable to find a g++ or clang++ in your PATH")
+
+    return os.path.realpath(cxx)
+
+
+def get_cc_version(cc) -> str:
+    cc_version = sp.check_output([cc, "-dumpversion"]).decode("ascii")
+    return re.search(r"^\d+", cc_version).group(0)
+
+
+def get_cmake_version(env) -> str:
+    res = sp.check_output(["cmake", "--version"], env=env).decode("utf-8")
+    matches = re.search(r"(\d+\.\d+.\d+)", res)
+
+    if not matches:
+        raise RuntimeError("Unable to infer cmake version")
+
+    return matches.group(1)
+
+
+def get_arch() -> str:
+    return platform.uname()[4].lower().replace("amd64", "x86_64")
+
+
 def run_conan_profile_detect_windows(env):
     assert os.name == "nt"
 
-    cc = env.get("CC")
-    gcc_version = sp.check_output([cc, "-dumpversion"]).decode("ascii")
-    gcc_version = re.search(r"^\d+", gcc_version).group(0)
+    env["PATH"] = get_path_as_r()
+
+    arch = get_arch()
+    cc = find_cc()
+    cxx = find_cxx()
+    cc_version = get_cc_version(cc)
+    cmake_version = get_cmake_version(env)
+
+    env["CC"] = cc
+    env["CXX"] = cxx
 
     profile = [
         "[settings]",
+        f"arch={arch}",
         "build_type=Release",
         "compiler=gcc",
-        "compiler.cppstd=gnu17",
-        f"compiler.version={gcc_version}",
+        "compiler.cppstd=17",
+        f"compiler.version={cc_version}",
         "os=Windows",
+        "[buildenv]",
+        f"PATH='" + env["PATH"] + "'",
+        "[platform_tool_requires]",
+        f"cmake/{cmake_version}",
     ]
 
     conan_profile = os.path.join(
-        env.get("CONAN_HOME", os.path.join(env.get("HOME"), ".conan2")),
+        get_conan_home(),
         "profiles",
         "default",
     )
 
     os.makedirs(os.path.dirname(conan_profile), exist_ok=True)
     with open(conan_profile, "w") as f:
-        print("\n".join(profile), file=f)
+        print("\n".join(profile), file=f, end="")
+
+    print("CONAN_HOME=" + get_conan_home(), file=sys.stderr)
+    print("CC=" + env.get("CC", ""), file=sys.stderr)
+    print("CXX=" + env.get("CXX", ""), file=sys.stderr)
 
 
 def run_conan_profile_detect(conan, env):
@@ -57,7 +184,7 @@ def run_conan_profile_detect(conan, env):
     sp.run([conan, "profile", "detect"], stdout=sp.DEVNULL, env=env)
 
     conan_profile = os.path.join(
-        env.get("CONAN_HOME", os.path.join(env.get("HOME"), ".conan2")),
+        env.get("CONAN_HOME", os.path.join(os.path.expanduser("~"), ".conan2")),
         "profiles",
         "default",
     )
@@ -67,11 +194,13 @@ def run_conan_profile_detect(conan, env):
 
 
 def run_conan_install(conan, env):
+    conanfile = os.path.join("..", "conanfile.txt")
+
     sp.check_call(
         [
             conan,
             "install",
-            os.path.join("..", "conanfile.txt"),
+            conanfile,
             "--settings=build_type=Release",
             "--settings=compiler.cppstd=17",
             "--output-folder=conan-staging",
@@ -95,13 +224,9 @@ def main():
         print(conandeps_mk)
         return
 
-    conan_home = env.get("CONAN_HOME")
+    conan_home = get_conan_home()
     if conan_home is not None:
         os.makedirs(conan_home, exist_ok=True)
-
-    print("CONAN_HOME=" + env.get("CONAN_HOME", ""), file=sys.stderr)
-    print("CC=" + env.get("CC", ""), file=sys.stderr)
-    print("CXX=" + env.get("CXX", ""), file=sys.stderr)
 
     run_conan_profile_detect(conan, env)
     run_conan_install(conan, env)
