c9fae4efd29bcbc44760a4d4bdb7465b78936f20
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-08-08 10:36:47 +0200

    Rework logic to build third-party dependencies using Conan

--- deps/generate_makevars.py
+++ deps/generate_makevars.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2024 Roberto Rossini <roberros@uio.no>
+# Copyright (C) 2025 Roberto Rossini <roberros@uio.no>
 #
 # SPDX-License-Identifier: MIT
 
 import argparse
+import logging
 import os
 import pathlib
 import platform
@@ -13,8 +14,9 @@ import shutil
 import subprocess as sp
 import sys
 import tempfile
+import textwrap
 import venv
-from typing import Dict, Optional, Tuple
+from typing import Dict, Tuple
 
 """
 This script takes care of generating a valid conan profile and running conan install.
@@ -26,6 +28,8 @@ This means that:
 - Make conan use the msys2 toolchain installed by Rtools
 """
 
+type EnvDict = Dict[str, str]
+
 
 def make_cli() -> argparse.ArgumentParser:
     cli = argparse.ArgumentParser()
@@ -34,7 +38,19 @@ def make_cli() -> argparse.ArgumentParser:
         "--force",
         action="store_true",
         default=False,
-        help="Run the script even if conandeps.mk already exists (default: %(default)s).",
+        help="Run the script even if Makevars already exists (default: %(default)s).",
+    )
+    cli.add_argument(
+        "--workdir",
+        type=pathlib.Path,
+        default=pathlib.Path().cwd(),
+        help="Working directory (default: %(default)s).",
+    )
+    cli.add_argument(
+        "--no-venv",
+        action="store_true",
+        default=False,
+        help="Do not run commands inside an ephemeral venv (default: %(default)s).",
     )
 
     return cli
@@ -47,7 +63,7 @@ def find_conan() -> pathlib.Path:
 
     conan = pathlib.Path(conan).absolute()
     version = sp.check_output([conan, "--version"])
-    print(f'found {version} at "{conan}"', file=sys.stderr)
+    logging.info('found %s at "%s"', version, conan)
 
     return conan
 
@@ -72,7 +88,7 @@ def get_rtools_home() -> pathlib.Path:
     if not rtools_home.exists():
         raise RuntimeError(f"Unable to find RTOOLS_HOME at: {rtools_home}")
 
-    print(f'Found Rtools at "{rtools_home}"', file=sys.stderr)
+    logging.info('Found Rtools at "%s"', rtools_home)
 
     return rtools_home
 
@@ -96,38 +112,44 @@ def get_path_as_r(add_rtools: bool = True) -> str:
     return ";".join(str(p) for p in path)
 
 
-def r_which(program: str) -> Optional[pathlib.Path]:
+def r_which(program: str, resolve: bool = True) -> pathlib.Path | None:
     res = sp.check_output(["Rscript", "-e", f'Sys.which("{program}")'], stderr=sp.DEVNULL).decode("utf-8")
 
     matches = re.search(r"\n\"(.*)\"", res, re.MULTILINE)
     if not matches:
         return None
 
-    return pathlib.Path(matches.group(1)).resolve()
+    exe = pathlib.Path(matches.group(1))
+    if resolve:
+        exe = exe.resolve()
+
+    return exe
 
 
 def find_cc() -> pathlib.Path:
-    cc = r_which("gcc")
+    for name in (os.getenv("CC", "cc"), "cc", "gcc", "clang"):
+        cc = r_which(name, resolve=False)
+        if cc is not None:
+            if "ccache" in str(cc):
+                return cc
+            return cc.resolve()
 
-    if cc == "":
-        cc = r_which("clang")
-
-    if cc == "":
-        raise RuntimeError("Unable to find a gcc or clang in your PATH")
-
-    return cc.resolve()
+    raise RuntimeError("Unable to find a gcc or clang in your PATH")
 
 
 def find_cxx() -> pathlib.Path:
-    cxx = r_which("g++")
+    for name in (os.getenv("CXX", "c++"), "c++", "g++", "clang++"):
+        cxx = r_which(name, resolve=False)
+        if cxx is not None:
+            if "ccache" in str(cxx):
+                return cxx
+            return cxx.resolve()
 
-    if cxx == "":
-        cxx = r_which("clang++")
+    raise RuntimeError("Unable to find a g++ or clang++ in your PATH")
 
-    if cxx == "":
-        raise RuntimeError("Unable to find a g++ or clang++ in your PATH")
 
-    return cxx.resolve()
+def cxx_flags_rcpp() -> str:
+    return sp.check_output(["Rscript", "-e", "Rcpp:::CxxFlags()"], stderr=sp.DEVNULL).decode("utf-8")
 
 
 def get_cc_version(cc) -> str:
@@ -135,7 +157,7 @@ def get_cc_version(cc) -> str:
     return re.search(r"^\d+", cc_version).group(0)
 
 
-def get_cmake_version(env) -> str:
+def get_cmake_version(env: EnvDict) -> str:
     res = sp.check_output(["cmake", "--version"], env=env).decode("utf-8")
     matches = re.search(r"(\d+\.\d+.\d+)", res)
 
@@ -149,7 +171,7 @@ def get_arch() -> str:
     return platform.uname()[4].lower().replace("amd64", "x86_64")
 
 
-def run_conan_profile_detect_windows(env):
+def run_conan_profile_detect_windows(env: EnvDict):
     assert os.name == "nt"
 
     env["PATH"] = str(get_path_as_r())
@@ -189,7 +211,7 @@ def run_conan_profile_detect_windows(env):
         print("\n".join(profile), file=f, end="")
 
 
-def run_conan_profile_detect(env):
+def run_conan_profile_detect(env: EnvDict):
     if os.name == "nt":
         run_conan_profile_detect_windows(env)
         return
@@ -201,16 +223,16 @@ def run_conan_profile_detect(env):
     )
 
     conan_profile = pathlib.Path(env.get("CONAN_HOME")) / "profiles" / "hictkR"
-    with conan_profile.open() as f:
-        for line in f:
-            print(line, file=sys.stderr, end="")
+    logging.info("\n%s", conan_profile.read_text())
 
 
-def run_conan_install(env):
-    hictk_conanfile = pathlib.Path().cwd().parent / "hictk.conanfile.py"
-    hictkR_conanfile = pathlib.Path().cwd().parent / "conanfile.txt"
-    assert hictk_conanfile.exists()
-    assert hictkR_conanfile.exists()
+def run_conan_install(
+    conanfile: pathlib.Path,
+    tmpdir: pathlib.Path,
+    env: EnvDict,
+) -> pathlib.Path:
+    assert conanfile.is_file()
+    assert tmpdir.is_dir()
 
     default_options = [
         "--profile:all=hictkR",
@@ -224,26 +246,136 @@ def run_conan_install(env):
     ]
 
     conan_install_opts = default_options + [
-        "--output-folder=conan-staging",
+        "--requires=hictk/[>=2.1 <2.2]",
+        f"--output-folder={tmpdir}",
         "--build=never",
         "--generator=MakeDeps",
-        "--generator=CMakeDeps",
     ]
 
     sp.check_call(
-        ["conan", "create", hictk_conanfile] + conan_create_opts,
+        ["conan", "create", conanfile] + conan_create_opts,
         stdout=sys.stderr,
         env=env,
     )
 
     sp.check_call(
-        ["conan", "install", hictkR_conanfile] + conan_install_opts,
+        ["conan", "install"] + conan_install_opts,
         stdout=sys.stderr,
         env=env,
     )
 
+    conandeps_mk = tmpdir / "conandeps.mk"
+    if not conandeps_mk.is_file():
+        raise RuntimeError(f"failed to create {conandeps_mk} file!")
 
-def get_or_init_conan_home(env: Optional[Dict[str, str]] = None) -> pathlib.Path:
+    return conandeps_mk
+
+
+def detect_filesystem_link_flag(tmpdir: pathlib.Path) -> str | None:
+
+    src_file = tmpdir / "filesystem_test.cpp"
+    test_program = tmpdir / "test.bin"
+
+    src_file.write_text(
+        textwrap.dedent(
+            """
+            #include <filesystem>
+
+            int main() {
+                return std::filesystem::path{}.empty() == false;
+            }
+            """
+        )
+    )
+
+    def try_compile(*args) -> bool:
+        cxx = find_cxx()
+        env = os.environ.copy()
+        env["CCACHE_DISABLE"] = "1"
+
+        cmd = [cxx, src_file, "-o", test_program, "-std=c++17", *args]
+
+        res = sp.run(cmd, env=env, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+
+        if res.returncode != 0:
+            return False
+
+        res = sp.run([test_program])
+
+        return res.returncode == 0
+
+    try:
+
+        if try_compile():
+            return None
+
+        for flag in ("-lc++fs", "-lstdc++fs"):
+            if try_compile(flag):
+                return flag
+
+        raise RuntimeError("Cannot compile a simple program using the std::filesystem library")
+
+    finally:
+        src_file.unlink()
+        test_program.unlink()
+
+
+def generate_makevars_unix(dest: pathlib.Path, tmpdir: pathlib.Path, conandeps_mk: pathlib.Path):
+    cc = find_cc()
+    cxx = find_cxx()
+    cxx_flags = cxx_flags_rcpp()
+
+    makevars = (
+        textwrap.dedent(
+            f"""
+        PWD := $(shell pwd)
+        TMPDIR := PWD
+
+        export CC := {cc}"
+        export CXX := {cxx}
+        export CXX17 = $(CXX)
+
+        ### BEGINNING OF conandeps.mk
+        """
+        )
+        + conandeps_mk.read_text(encoding="utf-8")
+        + textwrap.dedent(
+            f"""
+        ### END OF conandeps.mk
+
+        CXX_STD := CXX17
+
+        PKG_CPPFLAGS := $(addprefix -isystem ,$(CONAN_INCLUDE_DIRS))
+        PKG_CPPFLAGS := $(PKG_CPPFLAGS) $(addprefix -isystem ,$(CONAN_INCLUDE_DIRS_HDF5_HDF5_C))
+        PKG_CPPFLAGS := $(PKG_CPPFLAGS) $(addprefix -D ,$(CONAN_DEFINES))
+        PKG_CPPFLAGS := $(PKG_CPPFLAGS) {cxx_flags}
+
+        PKG_LIBS := $(addprefix -L ,$(CONAN_LIB_DIRS))
+        PKG_LIBS := $(PKG_LIBS) $(addprefix -l,$(CONAN_LIBS))
+        PKG_LIBS := $(PKG_LIBS) $(addprefix -l,$(CONAN_SYSTEM_LIBS))
+        """
+        )
+    )
+
+    filesystem_link_flags = detect_filesystem_link_flag(tmpdir)
+    if filesystem_link_flags is not None:
+        makevars += f"PKG_LIBS := $(PKG_LIBS) {filesystem_link_flags}"
+
+    dest.write_text(makevars)
+
+
+def generate_makevars_windows(dest: pathlib.Path, conandeps_mk: pathlib.Path):
+    pass
+
+
+def generate_makevars(dest: pathlib.Path, conandeps_mk: pathlib.Path, tmpdir: pathlib.Path):
+    if os.name == "nt":
+        generate_makevars_windows(dest, conandeps_mk)
+    else:
+        generate_makevars_unix(dest, tmpdir, conandeps_mk)
+
+
+def get_or_init_conan_home(env: EnvDict | None) -> pathlib.Path:
     if env is None:
         env = os.environ.copy()
 
@@ -258,9 +390,9 @@ def get_or_init_conan_home(env: Optional[Dict[str, str]] = None) -> pathlib.Path
     return conan_home
 
 
-def setup_venv(tmpdir: pathlib.Path) -> Tuple[pathlib.Path, Dict[str, str]]:
+def setup_venv(tmpdir: pathlib.Path) -> Tuple[pathlib.Path, EnvDict]:
     venv_path = tmpdir / "venv"
-    print(f'creating venv under "{venv_path}"...', file=sys.stderr)
+    logging.info('creating venv under "%s"...', venv_path)
     venv.create(venv_path, with_pip=True, upgrade_deps=True)
 
     env = os.environ.copy()
@@ -283,38 +415,50 @@ def setup_venv(tmpdir: pathlib.Path) -> Tuple[pathlib.Path, Dict[str, str]]:
     return venv_path, env
 
 
+def setup_logger(level: str = "INFO"):
+    fmt = "[%(asctime)s] %(levelname)s: %(message)s"
+    logging.basicConfig(format=fmt)
+    logging.getLogger().setLevel(level)
+
+
 def main():
     args = make_cli().parse_args()
 
-    pwd = pathlib.Path().cwd()
-    conandeps_mk = pwd / "conan-staging" / "conandeps.mk"
+    workdir = args.workdir
+
+    makevars_file = workdir / "src" / "Makevars"
 
     if args.force:
-        if conandeps_mk.parent.exists():
-            shutil.rmtree(conandeps_mk.parent)
+        makevars_file.unlink(missing_ok=True)
 
-    if conandeps_mk.exists():
-        print(conandeps_mk)
+    if makevars_file.exists():
+        logging.debug("\n%s", makevars_file.read_text())
+        logging.info('found existing Makevars file at "%s"!', makevars_file.resolve())
         return
 
     with tempfile.TemporaryDirectory() as tmpdir:
         tmpdir = pathlib.Path(tmpdir)
-        _, env = setup_venv(tmpdir)
+        if args.no_venv:
+            logging.info("skipping venv creation...")
+            env: EnvDict = os.environ.copy()
+        else:
+            _, env = setup_venv(tmpdir)
 
         conan_home = get_or_init_conan_home(env)
 
-        print(f"CONAN_HOME={conan_home}", file=sys.stderr)
+        logging.info("CONAN_HOME=%s", conan_home)
         env["CONAN_HOME"] = str(conan_home)
         env["TMPDIR"] = str(tmpdir.absolute())
 
         run_conan_profile_detect(env)
-        run_conan_install(env)
+        conandeps_mk = run_conan_install(workdir / "deps" / "conanfile.py", tmpdir, env)
 
-        if not conandeps_mk.is_file():
-            raise RuntimeError(f"failed to create {conandeps_mk} file!")
+        generate_makevars(makevars_file, conandeps_mk, tmpdir)
 
-        print(conandeps_mk)
+        logging.debug("\n%s", makevars_file.read_text())
+        logging.info('Makevars file has been written to "%s"...', makevars_file.resolve())
 
 
 if __name__ == "__main__":
+    setup_logger()
     main()
